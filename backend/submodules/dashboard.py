2cd14e880c5443a03bc90dde74a85bdb118c67ed
abelsam-coder <abelsamuel841@gmail.com>
2025-08-13 14:11:26 -0700

    Update dashboard.py

--- backend/submodules/dashboard.py
+++ backend/submodules/dashboard.py
@@ -21,7 +21,11 @@ def hom():
         n = "false"  
     c.execute("SELECT image FROM profile WHERE name = ?",(usernam,))
     print(c.fetchone())
-    profile = c.fetchone()[0]
+    if c.fethcone():
+        profile = c.fetchone()[0]
+    else:
+        with open("static/t.txt","r") as k:
+            profile = k.read()
     c.execute("SELECT COUNT(*) FROM follow WHERE username = ?",(usernam,))
     followers = c.fetchone()
     for followers in followers:
@@ -92,6 +96,7 @@ def hom():
                 })
    
     return render_template("dashboard.html",f=a,profile=profile,username=usernam,followers=follow,ff=followerr,new=n)     
+
 
 
 
